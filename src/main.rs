ec8d53324f47d821dab51dff9983faa1799fe74f
siph <dawkins.chris.dev@gmail.com>
2022-04-18 16:50:54 -0600

    feature: http server init

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,18 @@
+use anyhow::{Context, Result, Ok};
 use clap::Parser;
-use dotenv::dotenv;
+use sqlx::postgres::PgPoolOptions;
 use url_wrapper::config::Config;
 
-fn main() {
+#[tokio::main]
+async fn main() -> Result<()> {
     dotenv::dotenv().ok();
     env_logger::init();
+    let config = Config::parse();
+    let db = PgPoolOptions::new()
+        .max_connections(50)
+        .connect(&config.database_url)
+        .await
+        .context("could not connect to database")?;
+    sqlx::migrate!().run(&db).await?;
+    Ok(())
 }
