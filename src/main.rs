046e7dab0ca1718a339567ebbf729588870d0693
siph <dawkins.chris.dev@gmail.com>
2022-04-26 00:34:01 -0600

    feature: basic server

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,32 @@
-use entity::links;
+use std::net::SocketAddr;
+use std::str::FromStr;
+
+use axum::{Router, AddExtensionLayer, Server};
+use tower::ServiceBuilder;
+use url_wrapper::config::Config;
+use clap::Parser;
+use sea_orm::Database;
+use migration::{Migrator, MigratorTrait};
 
 #[tokio::main]
-async fn main() -> Result<(), ()> {
+async fn main() -> anyhow::Result<()> {
+    dotenv::dotenv().ok();
+    env_logger::init();
+    let config = Config::parse();
+    let conn = Database::connect(&config.database_url)
+        .await
+        .expect("Database connection failed");
+    Migrator::up(&conn, None).await.unwrap();
+    let app = Router::new()
+        // .route("/delete/:id", post(delete_post))
+        .layer(
+            ServiceBuilder::new()
+                .layer(AddExtensionLayer::new(conn))
+        );
+    let server_url = format!("{}:{}", &config.ip, &config.port);
+    let addr = SocketAddr::from_str(&server_url).unwrap();
+    Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await?;
     Ok(())
 }
